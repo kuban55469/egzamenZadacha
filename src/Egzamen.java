ee16d3535275258c611efb1bc2502fcfa9f41cce
lesson1 <kubanmuit@gmail.com>
2022-10-31 16:59:14 +0600

    Initial commit

---
+++ src/Egzamen.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Egzamen {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Сан жазыныз: ");
+        int num = sc.nextInt();
+        System.out.print("Экинчи санды жазыныз: ");
+        int num2 = sc.nextInt();
+        int counter = 0, i = 0;
+        counter = num-num2;
+        System.out.println("Кемитиндиси: " + counter);
+        while (counter > 0) {
+            System.out.print("Сан жазыныз: ");
+            int num3 = sc.nextInt();
+            i = counter - num3;
+            System.out.println("Кемитиндиси: " + i);
+            counter-=num3;
+            if (counter == 0){
+                System.out.println("Нолго жеттик!!");
+            }
+        }
+
+    }
+}
